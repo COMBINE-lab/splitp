24cea51e02f7db7d04224d7b6853bac366239c99
rob-p <rob.patro@gmail.com>
2021-11-18 13:37:11 -0500

    Moved functions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use serde::Deserialize;
 use std::collections::HashMap;
 use std::io::Write;
 use std::str;
+mod utils;
 
 // first, reproduce the appproach from
 // https://github.com/jeremymsimon/SPLITseq/blob/main/Preprocess_SPLITseq_collapse_bcSharing.pl
@@ -35,41 +36,6 @@ struct Opts {
 struct BCMapRecord {
     oligo_dt: String,
     rand_hex: String,
-}
-
-fn get_bit_mask(nt_index: usize, fill_with: u64) -> u64 {
-    let mut mask: u64 = fill_with;
-    mask <<= 2 * (nt_index - 1);
-    mask
-}
-
-fn get_all_snps(bc: u64, bc_length: usize) -> Vec<u64> {
-    assert!(
-        bc <= 2u64.pow(2 * bc_length as u32),
-        "the barcode id is larger than possible (based on barcode length)"
-    );
-    assert!(
-        bc_length <= 32,
-        "barcode length greater than 32 not supported"
-    );
-
-    let mut snps: Vec<u64> = Vec::new();
-    snps.reserve(3 * bc_length);
-
-    for nt_index in 1..=bc_length {
-        // clearing the two relevant bits based on nucleotide position
-        let bit_mask = bc & !get_bit_mask(nt_index, 3);
-
-        // iterating over all 4 choices of the nucleotide
-        for i in 0..=3 {
-            let new_bc = bit_mask | get_bit_mask(nt_index, i);
-            if new_bc != bc {
-                snps.push(new_bc);
-            }
-        }
-    }
-
-    snps
 }
 
 
@@ -120,7 +86,7 @@ fn parse_bc_map(bc_map: &str, one_edit: bool) -> HashMap<u64, String, ahash::Ran
             // for insertion into the map with the corresponding 
             // oligo-dT.
             if one_edit {
-                for n in get_all_snps(rh.0, record.rand_hex.len()) {
+                for n in utils::get_all_snps(rh.0, record.rand_hex.len()) {
                     let count = hs.entry(n).or_insert(0usize);
                     if *count > 0 {
                         collisions += 1;
